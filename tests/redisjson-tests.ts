ad2da56861d40f1418e1e63bfda5e5927441e6d1
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-10-21 01:00:25 +0300

    .

---
+++ tests/redisjson-tests.ts
@@ -0,0 +1,70 @@
+import { cliArguments } from 'cli-argument-parser';
+import { expect } from 'chai'
+import { Redisjson } from '../modules/redisjson';
+const client = new Redisjson({
+    host: 'localhost',
+    port: 123,
+    password: '123951'
+});
+
+describe('Sanity testing', async function() {
+    it('delCommand function', async () => {
+    });
+
+    it(' function', async () => {
+    });
+
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+    
+    it(' function', async () => {
+    });
+});
