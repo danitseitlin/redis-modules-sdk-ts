0837529385fe5a81d58ffb7562aa7cf5978e2d9f
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-10-25 01:25:39 +0300

    .

--- tests/rejson.ts
+++ tests/rejson.ts
@@ -35,74 +35,75 @@ describe('RedisJSON Module testing', async function() {
     
     it('mgetCommand function', async () => {
         const response = await client.mgetCommand([key1, key2], path);
-        expect(response).to.equal('[ \'{"x":1,"str":"yy"}\', \'{"x":3}\' ]');
+        expect(response).to.equal('[ \'{"x":1,"str":"yy"}\', \'{"x":3}\' ]', 'The response of the mget command');
     });
     
     it('typeCommand function', async () => {
-        console.log(await client.typeCommand(key1, path));
+        const response = await client.typeCommand(key1, path);
+        expect(response).to.equal('object', 'The response of the type command')
     });
     
     it('numincrbyCommand function', async () => {
-        console.log(await client.numincrbyCommand(key1, 2, '.x'));
+        const response = await client.numincrbyCommand(key1, 2, '.x');
     });
     
     it('nummultbyCommand function', async () => {
-        console.log(await client.nummultbyCommand(key1, 3, '.x'));
+        const response = await client.nummultbyCommand(key1, 3, '.x');
     });
     
     it('strappendCommand function', async () => {
-        console.log(await client.strappendCommand(key1, '"rrr"', '.str'));
+        const response = await client.strappendCommand(key1, '"rrr"', '.str');
     });
     
     it('strlenCommand function', async () => {
-        console.log(await client.strlenCommand(key1, '.str'))
+        const response = await client.strlenCommand(key1, '.str')
     });
     
     it('arrappendCommand function', async () => {
-        console.log(await client.arrappendCommand(key3, [3,5,4,2], '.items'))
+        const response = await client.arrappendCommand(key3, [3,5,4,2], '.items')
     });
     
     it('arrindexCommand function', async () => {
-        console.log(await client.arrindexCommand(key3, '1', '.items'));
+        const response = await client.arrindexCommand(key3, '1', '.items');
     });
     
     it('arrinsertCommand function', async () => {
-        console.log(await client.arrinsertCommand(key3, 1, '{"z": 5}', '.items'));
+        const response = await client.arrinsertCommand(key3, 1, '{"z": 5}', '.items');
     });
     
     it('arrlenCommand function', async () => {
-        console.log(await client.arrlenCommand(key3, '.items'));
+        const response = await client.arrlenCommand(key3, '.items');
     });
     
     it('arrpopCommand function', async () => {
-        console.log(await client.arrpopCommand(key3, 0, '.items'));
+        const response = await client.arrpopCommand(key3, 0, '.items');
     });
     
     it('arrtrimCommand function', async () => {
-        console.log(await client.arrtrimCommand(key3, 0, 1, '.items'));
+        const response = await client.arrtrimCommand(key3, 0, 1, '.items');
     });
     
     it('objkeysCommand function', async () => {
-        console.log(await client.objkeysCommand(key1, path));
+        const response = await client.objkeysCommand(key1, path);
     });
     
     it('objlenCommand function', async () => {
-        console.log(await client.objlenCommand(key1, path));
+        const response = await client.objlenCommand(key1, path);
     });
     
     it('debugCommand function', async () => {
-        console.log(await client.debugCommand('MEMORY', key1, path));
+        const response = await client.debugCommand('MEMORY', key1, path);
     });
     
     it('forgetCommand function', async () => {
-        console.log(await client.forgetCommand(key2, path))
+        const response = await client.forgetCommand(key2, path)
     });
     
     it('respCommand function', async () => {
-        console.log(await client.respCommand(key1, path))
+        const response = await client.respCommand(key1, path)
     });
 
     it('delCommand function', async () => {
-        console.log(await client.delCommand(key1, path));
+        const response = await client.delCommand(key1, path);
     });
 });
