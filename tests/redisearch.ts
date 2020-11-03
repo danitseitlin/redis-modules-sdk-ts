3b5854f86670c3676efee513704b0cc7b20792d8
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-03 22:09:48 +0200

    >

--- tests/redisearch.ts
+++ tests/redisearch.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import { RediSearch } from '../modules/redisearch';
 let client: RediSearch;
 let index = 'idx'
-let query = '@text:name=>{$phonetic:false}'
+let query = '@text:name'
 describe('RediSearch Module testing', async function() {
     before(async () => {
         client = new RediSearch({
