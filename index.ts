4bf7671a48b5f88207a3e30f1d3c213a19b00577
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-08 08:43:28 +0200

    Exporting Redisearch Class

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ export {
     RedisTimeSeries, TSCreateOptions, TSLabel, TSAddOptions, TSKeySet, TSIncrbyDecrbyOptions, TSOptions, TSCreateRule, TSAggregationType, TSRangeOptions, TSMRangeOptions, Info
 } from './modules/rts';
 export {
-    FTCreateParameters, FTFieldOptions, FTSchemaField, FTSearchParameters, FTAggregateParameters, FTSugAddParameters, FTSugGetParameters, FTSpellCheck, FTFieldType
+    Redisearch, FTCreateParameters, FTFieldOptions, FTSchemaField, FTSearchParameters, FTAggregateParameters, FTSugAddParameters, FTSugGetParameters, FTSpellCheck, FTFieldType
 } from './modules/redisearch'
