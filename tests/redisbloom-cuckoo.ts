e1ad2586e242d7b669c095f907f92fbd82ad5760
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-23 06:38:47 +0200

    >

--- tests/redisbloom-cuckoo.ts
+++ tests/redisbloom-cuckoo.ts
@@ -21,14 +21,17 @@ describe('RedisBloom Cuckoo filter testing', async function() {
     it('add function', async () => {
         const response = await client.add(key1, 'item');
         console.log(response)
+        expect(response).to.equal(1, 'The response of the CF.ADD command');
     });
     it('addnx function', async () => {
         const response = await client.addnx(key1, 'item1');
         console.log(response)
+        expect(response).to.equal(1, 'The response of the CF.ADDNX command');
     });
     it('insert function', async () => {
         const response = await client.insert(key1, ['item4', 'item5'])
         console.log(response)
+        expect(response[0]).to.equal(1, 'The response of the CF.INSERT command');
     });
     it('insertnx function', async () => {
         const response = await client.insertnx(key1, ['item6'])
@@ -37,25 +40,33 @@ describe('RedisBloom Cuckoo filter testing', async function() {
     it('exists function', async () => {
         const response = await client.exists(key1, 'item1');
         console.log(response)
+        expect(response).to.equal(1, 'The response of the CF.EXISTS command');
     });
     it('count function', async () => {
         const response = await client.count(key1, 'item1');
         console.log(response)
+        expect(response).to.equal(1, 'The response of the CF.COUNT command');
     });
     it('scandump function', async () => {
         const response = await client.scandump(key1, 123)
         console.log(response)
+        expect(response).to.equal(1, 'The response of the CF.SCANDUMP command');
     });
     it('loadchunk function', async () => {
-        const response = await client.loadchunk(key1, dataIterator, data)
+        const response = await client.loadchunk(key1, dataIterator, data);
         console.log(response)
+        dataIterator = parseInt(response[0])
+        data = response[1];
     });
     it('info function', async () => {
         const response = await client.info(key1);
         console.log(response)
+        expect(response[1]).to.equal(1080, 'The size of the key');
+        expect(response[3]).to.equal(512, 'The number of buckets of the key');
     });
     it('del function', async () => {
         const response = await client.del(key1, 'item1');
         console.log(response)
+        expect(response).to.equal(1, 'The response of the CF.DEL command');
     });
 });
