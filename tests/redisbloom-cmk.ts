5408187a49d0988348db2a204e01248018d86b37
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-23 06:30:02 +0200

    >

--- tests/redisbloom-cmk.ts
+++ tests/redisbloom-cmk.ts
@@ -25,7 +25,6 @@ describe('RedisBloom Count-Min-Sketch filter testing', async function() {
     });
     it('initbyprob function', async () => {
         const response = await client.initbyprob(key2, 0.001, 0.01);
-        console.log(response)
         expect(response).to.equal('OK', 'The response of CMS.INITBYPROB command');
     });
     it('incrby function', async () => {
@@ -33,22 +32,18 @@ describe('RedisBloom Count-Min-Sketch filter testing', async function() {
             name: 'foo',
             increment: 10
         }]);
-        console.log(response)
         expect(response[0]).to.equal(10, 'The response of CMS.INCRBY command');
     });
     it('query function', async () => {
         const response = await client.query(key1, ['foo']);
-        console.log(response)
         expect(response[0]).to.equal(10, 'The response of CMS.QUERY command');
     });
     it('merge function', async () => {
         const response = await client.merge('dest', 1, [key1]);
-        console.log(response)
         expect(response).to.equal('OK', 'The response of CMS.MERGE command');
     });
     it('info function', async () => {
         const response = await client.info(key1);
-        console.log(response);
         expect(response[1]).to.equal(1, 'The width of the key');
         expect(response[3]).to.equal(2, 'The depth of the key');
         expect(response[5]).to.equal(10, 'The count of the key');
--- tests/redisbloom-cuckoo.ts
+++ tests/redisbloom-cuckoo.ts
@@ -1,7 +1,10 @@
 import { cliArguments } from 'cli-argument-parser';
 import { expect } from 'chai'
 import { RedisBloomCuckoo } from '../modules/redisbloom-cuckoo';
+const key1 = 'key1cuckoo'
 let client: RedisBloomCuckoo;
+let dataIterator: number;
+let data: string;
 
 describe('RedisBloom Cuckoo filter testing', async function() {
     before(async () => {
@@ -16,23 +19,43 @@ describe('RedisBloom Cuckoo filter testing', async function() {
     })
 
     it('add function', async () => {
+        const response = await client.add(key1, 'item');
+        console.log(response)
     });
     it('addnx function', async () => {
+        const response = await client.addnx(key1, 'item1');
+        console.log(response)
     });
     it('insert function', async () => {
+        const response = await client.insert(key1, ['item4', 'item5'])
+        console.log(response)
     });
     it('insertnx function', async () => {
+        const response = await client.insertnx(key1, ['item6'])
+        console.log(response)
     });
     it('exists function', async () => {
+        const response = await client.exists(key1, 'item1');
+        console.log(response)
     });
     it('count function', async () => {
+        const response = await client.count(key1, 'item1');
+        console.log(response)
     });
     it('scandump function', async () => {
+        const response = await client.scandump(key1, 123)
+        console.log(response)
     });
     it('loadchunk function', async () => {
+        const response = await client.loadchunk(key1, dataIterator, data)
+        console.log(response)
     });
     it('info function', async () => {
+        const response = await client.info(key1);
+        console.log(response)
     });
     it('del function', async () => {
+        const response = await client.del(key1, 'item1');
+        console.log(response)
     });
 });
