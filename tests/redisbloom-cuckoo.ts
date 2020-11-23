20b1b927017d925e8202c2d2008c73db9c2f9631
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-23 06:46:30 +0200

    .

--- tests/redisbloom-cuckoo.ts
+++ tests/redisbloom-cuckoo.ts
@@ -2,6 +2,8 @@ import { cliArguments } from 'cli-argument-parser';
 import { expect } from 'chai'
 import { RedisBloomCuckoo } from '../modules/redisbloom-cuckoo';
 const key1 = 'key1cuckoo'
+const key2 = 'key2cuckoo'
+const key3 = 'key3cuckoo'
 let client: RedisBloomCuckoo;
 let dataIterator: number;
 let data: string;
@@ -34,8 +36,9 @@ describe('RedisBloom Cuckoo filter testing', async function() {
         expect(response[0]).to.equal(1, 'The response of the CF.INSERT command');
     });
     it('insertnx function', async () => {
-        const response = await client.insertnx(key1, ['item6'])
+        const response = await client.insertnx(key3, ['item'])
         console.log(response)
+        expect(response[0]).to.equal(1, 'The response of the CF.INSERTNX command');
     });
     it('exists function', async () => {
         const response = await client.exists(key1, 'item1');
@@ -48,12 +51,14 @@ describe('RedisBloom Cuckoo filter testing', async function() {
         expect(response).to.equal(1, 'The response of the CF.COUNT command');
     });
     it('scandump function', async () => {
+        await client.add(key2, 'item');
+        await client.redis.del(key2);
         const response = await client.scandump(key1, 123)
         console.log(response)
         expect(response).to.equal(1, 'The response of the CF.SCANDUMP command');
     });
     it('loadchunk function', async () => {
-        const response = await client.loadchunk(key1, dataIterator, data);
+        const response = await client.loadchunk(key2, dataIterator, data);
         console.log(response)
         dataIterator = parseInt(response[0])
         data = response[1];
