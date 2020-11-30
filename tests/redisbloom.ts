395c2c1a0dbf3d229724be80733a23d57216020c
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-30 10:45:51 +0200

    >

--- tests/redisbloom.ts
+++ tests/redisbloom.ts
@@ -53,6 +53,7 @@ describe('RedisBloom Module testing', async function() {
         //responses = [];
         let response = await client.scandump(key2, 0)
         console.log(response)
+        console.log(Buffer.from(response[1]).toString('binary'))
         dataIterator = parseInt(response[0])
         expect(dataIterator).to.equal(1, 'The chunk data iterator');
         while(parseInt(response[0]) > 0){
