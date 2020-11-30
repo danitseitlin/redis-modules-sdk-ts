6d45a124b0fd78d2d51d7adf834c80b9652045eb
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-11-30 17:36:10 +0200

    >

--- modules/redisbloom.ts
+++ modules/redisbloom.ts
@@ -89,7 +89,7 @@ export class RedisBloom extends Module {
      * @param key The key of the 'BF.SCANDUMP' command
      * @param iterator The iterator of the 'BF.SCANDUMP' command
      */
-    async scandump(key: string, iterator: number): Promise<any[]> {
+    async scandump(key: string, iterator: number): Promise<string[]> {
         return await this.redis.send_command('BF.SCANDUMP', [key, iterator])
     }
 
