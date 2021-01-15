4e698171553fe401159e5053547a3ce6ffd2ade5
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-01-16 00:56:18 +0200

    >

--- modules/module.base.ts
+++ modules/module.base.ts
@@ -42,6 +42,7 @@ export class Module {
      * @param keys The keys of the key-value JS object
      */
     convertArrayResponseToJson(response: string[]): {[key: string]: (string | number)} {
+        console.log('converting...')
         const obj = {}
         for(let i = 0; i < response.length; i+2) {
             if(response[i+1] !== '' && response[i+1] !== undefined)
