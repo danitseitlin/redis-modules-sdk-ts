daf7585f964e6544498edd7972528a38c0209cc6
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-01-16 23:05:52 +0200

    >

--- tests/redisbloom.ts
+++ tests/redisbloom.ts
@@ -90,7 +90,8 @@ describe('RedisBloom Module testing', async function() {
         const data = "\x03\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x05\x00\x00\x00\x02\x00\x00\x00\x90\x00\x00\x00\x00\x00\x00\x00\x80\x04\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00{\x14\xaeG\xe1zt?\xe9\x86/\xb25\x0e&@\b\x00\x00\x00d\x00\x00\x00\x00\x00\x00\x00\x00";
         console.log(data)
         const buffer = Buffer.from(data, 'binary');
-        console.log(buffer.toString())
+        console.log(buffer.toString('ascii'))
+        console.log(buffer.toString('binary'))
         console.log(await client.loadchunk('2', 1, buffer));
         //for(const res of responses) {
         //    //console.log(`\n=== ${res[0]} ===`)
