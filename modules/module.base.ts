4ec4651c3b2af1542d938a46238cfc7201680959
Dani Tseitlin <39850374+danitseitlin@users.noreply.github.com>
2021-02-16 23:23:11 +0200

    Object based Redisearch response (#50)

--- modules/module.base.ts
+++ modules/module.base.ts
@@ -38,6 +38,57 @@ export class Module {
     }
 
     /**
+     * Simpilizing the response of the Module command
+     * @param response The array response from the module
+     */
+    handleResponse(response: any): any {
+        const obj = {}
+        //If not an array/object
+        if(
+            typeof response === 'string' ||
+            typeof response === 'number' ||
+            (Array.isArray(response) && response.length % 2 === 1 && response.length > 1 && !this.isOnlyTwoDimensionalArray(response)) ||
+            (Array.isArray(response) && response.length === 0)
+        ) return response;
+        else if(Array.isArray(response) && response.length === 1)
+            return this.handleResponse(response[0])
+        else if(Array.isArray(response)  && response.length > 1 && this.isOnlyTwoDimensionalArray(response))
+            return this.handleResponse(this.reduceArrayDimension(response))
+        //If is an array/obj we will build it
+        for(let i = 0; i < response.length; i+=2) {
+            if(response[i+1] !== '' && response[i+1] !== undefined) {
+                if(Array.isArray(response[i+1]) && this.isOnlyTwoDimensionalArray(response[i+1])) {
+                    obj[response[i]] = this.reduceArrayDimension(response[i+1]);
+                    continue;
+                }
+                const value = (Array.isArray(response[i+1]) ? this.handleResponse(response[i+1]): response[i+1])
+                obj[response[i]] = value;
+            }
+        }
+        return obj
+    }
+
+    /**
+     * Check if array is fully two dimensional. Only items in the array are arrays.
+     * @param array The potential two dimensional array
+     */
+    isOnlyTwoDimensionalArray(array: any[]): boolean {
+        return array.filter(item => Array.isArray(item)).length === array.length;
+    }
+
+    /**
+     * Reducing an array by one level. i.e. from two dimensional to 1 dimensional.
+     * @param array The potentional two dimensional array
+     */
+    reduceArrayDimension(array: any[][]): any[] {
+        let newArray = [];
+        array.forEach(singleArr => {
+            newArray = newArray.concat(singleArr)
+        })
+        return newArray;
+    }
+
+    /**
      * Converting an array to a key-value JS object based on response
      * @param response The array response from the module
      * @param keys The keys of the key-value JS object
