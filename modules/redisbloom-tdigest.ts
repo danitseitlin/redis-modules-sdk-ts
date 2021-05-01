c08ff58e53fd3fdc03b69390261cb16b944a26e9
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-05-01 20:06:43 +0300

    WIP

---
+++ modules/redisbloom-tdigest.ts
@@ -0,0 +1,167 @@
+import * as Redis from 'ioredis';
+import { Module, RedisModuleOptions } from './module.base';
+
+export class RedisBloomTDigest extends Module {
+
+    /**
+     * Initializing the RedisBloom Count-Min Sketch object
+     * @param options The options of the Redis database.
+     * @param moduleOptions The additional module options
+     * @param moduleOptions.isHandleError If to throw error on error
+     * @param moduleOptions.showDebugLogs If to print debug logs
+     */
+    constructor(options: Redis.RedisOptions, public moduleOptions?: RedisModuleOptions) {
+        super(RedisBloomTDigest.name, options, moduleOptions)
+    }
+
+    /**
+     * Allocate the memory and initialize the t-digest
+     * @param key The name of the sketch
+     * @param compression The compression parameter. 100 is a common value for normal uses. 1000 is extremely large. See the further notes bellow. 
+     * @returns 'OK' on success
+     */
+    async create(key: string, compression: number): Promise<'OK'> {
+        try {
+            return await this.sendCommand('TDIGEST.CREATE', [key, `${compression}`]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Reset the sketch to zero - empty out the sketch and re-initialize it
+     * @param key The name of the sketch
+     * @returns 'OK' on success
+     */
+    async reset(key: string): Promise<'OK'> {
+        try {
+            return await this.sendCommand('TDIGEST.RESET', [key]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Adds one or more samples to a sketch
+     * @param key The name of the sketch
+     * @param parameters The parameters of the command
+     * @returns 
+     */
+    async add(key: string, parameters: TDigestAddParameters[]): Promise<'OK'> {  
+        try {
+            let args = [key]
+            for(const pair of parameters)
+                args = args.concat([pair.value, `${pair.weight}`])
+            return await this.sendCommand('TDIGEST.ADD', args);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Merges all of the values from 'from' to 'this' sketch
+     * @param fromKey Sketch to copy values to
+     * @param toKey Sketch to copy values from
+     * @returns 
+     */
+    async merge(fromKey: string, toKey: string): Promise<'OK'> {
+        try {
+            return await this.sendCommand('TDIGEST.MERGE', [toKey, fromKey]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Get minimum value from the sketch. Will return DBL_MAX if the sketch is empty
+     * @param key The name of the sketch
+     * @returns 
+     */
+    async min(key: string): Promise<number> {
+        try {
+            return await this.sendCommand('TDIGEST.MIN', [key]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Get maximum value from the sketch. Will return DBL_MIN if the sketch is empty
+     * @param key The name of the sketch
+     * @returns 
+     */
+    async max(key: string): Promise<number> {
+        try {
+            return await this.sendCommand('TDIGEST.MAX', [key]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Returns an estimate of the cutoff such that a specified fraction of the data added to this TDigest would be less than or equal to the cutoff
+     * @param key The name of the sketch
+     * @param quantile 
+     * @returns 
+     */
+    async quantile(key: string, quantile: string) {
+        try {
+            return await this.sendCommand('TDIGEST.QUANTILE', [key, quantile]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Returns the fraction of all points added which are <= value
+     * @param key The name of the sketch
+     * @param value 
+     */
+    async cdf(key: string, value: string) {
+        try {
+            return await this.sendCommand('TDIGEST.CDF', [key, value]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+
+    /**
+     * Returns compression, capacity, total merged and unmerged nodes, the total compressions made up to date on that key, and merged and unmerged weight.
+     * @param key The name of the sketch
+     */
+    async info(key: string) {
+        try {
+            return await this.sendCommand('TDIGEST.INFO', [key]);
+        }
+        catch(error) {
+            return this.handleError(error);
+        }
+    }
+}
+
+
+/**
+ * 
+ */
+export type TDigestAddParameters = {
+    value: string,
+    weight: number
+}
+
+/**
+ * The sets of the incrby items (and increments)
+ * @param name The item name which counter to be increased.
+ * @param increment The counter to be increased by this integer.
+ */
+export type CMKIncrbyItems = {
+    name: string,
+    increment: number
+}
