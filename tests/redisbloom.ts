df1555f1a7ab0689ae91891e854fe7623173883e
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-05-17 20:53:29 +0300

    >

--- tests/redisbloom.ts
+++ tests/redisbloom.ts
@@ -7,9 +7,7 @@ let redis: Redis;
 const key1 = 'key1bloom';
 const key2 = '1';
 const item1 = 'item1';
-const responses = []
-let dataIterator: number;
-let data: string;
+const chunks: {iterator: number, data: string}[] = [];
 
 describe('RedisBloom Module testing', async function() {
     before(async () => {
@@ -59,23 +57,6 @@ describe('RedisBloom Module testing', async function() {
         expect(response[1]).to.equal(100, 'The value of the \'Capacity\' item')
     });
     it('scandump function', async () => {
-        //responses = [];
-
-//         127.0.0.1:6379> BF.RESERVE 1 0.01 100
-// OK
-// 127.0.0.1:6379> BF.ADD 1 1
-// (integer) 1
-// 127.0.0.1:6379> BF.ADD 1 2
-// (integer) 1
-// 127.0.0.1:6379> BF.ADD 1 3
-// (integer) 1
-// 127.0.0.1:6379> BF.SCANDUMP 1 0
-// 1) (integer) 1
-// 2) "\x03\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x05\x00\x00\x00\x02\x00\x00\x00\x90\x00\x00\x00\x00\x00\x00\x00\x80\x04\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00{\x14\xaeG\xe1zt?\xe9\x86/\xb25\x0e&@\b\x00\x00\x00d\x00\x00\x00\x00\x00\x00\x00\x00
-//    "\x03\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x05\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00{\x14�G�zt?�/�5\x0E&@\b\x00\x00\x00d\x00\x00\x00\x00\x00\x00\x00\x00"   
-        // await client.add(key2, '1')
-        // await client.add(key2, '2')
-        // await client.add(key2, '3')
         let iter = 0;
         let response = await client.scandump(key2, iter)
         let data = response[1]
@@ -87,48 +68,11 @@ describe('RedisBloom Module testing', async function() {
             data = response[1]
             chunks.push({iterator: iter, data: data})
         }
-
-        console.log(chunks)
-
-        // console.log(await client.redis.del(key2));
-        // console.log(await client.redis.get(key2));
+        expect(chunks.length).gt(0, `The count of chunks of key ${key2}`)
+    });
+    it('loadchunk function', async () => {
         const chunk = chunks[1];
         const res = await client.loadchunk(key2, chunk.iterator, chunk.data);
-            expect(res).to.equal('OK', `The response of load chunk with iterator ${chunk.iterator}`)
-        // for(const chunk of chunks) {
-        //     console.log(chunk)
-        //     const res = await client.loadchunk(key1, chunk.iterator, chunk.data);
-        //     expect(res).to.equal('OK', `The response of load chunk with iterator ${chunk.iterator}`)
-        // }
-
-        // chunks = []
-        // iter = 0
-        // while True:
-        //     iter, data = BF.SCANDUMP(key, iter)
-        //     if iter == 0:
-        //         break
-        //     else:
-        //         chunks.append([iter, data])
-
-        // # Load it back
-        // for chunk in chunks:
-        //     iter, data = chunk
-        //     BF.LOADCHUNK(key, iter, data)
-    });
-    it.skip('loadchunk function', async () => {
-        await client.redis.del(key2);
-        for(const res of responses) {
-            console.log(`\n=== ${res[0]} ===`)
-            console.log(Buffer.from(res[1], 'ascii').toString('hex'))
-            console.log(Buffer.from(res[1], 'ascii').toString('ascii'))
-            console.log(Buffer.from(res[1], 'ascii').toString('base64'))
-            console.log(Buffer.from(res[1], 'ascii').toString('binary'))
-            console.log(Buffer.from(res[1], 'ascii').toString('utf-8'))
-            console.log(Buffer.from(res[1], 'ascii').toString('utf8'))
-            console.log(Buffer.from(res[1], 'ascii').toString('utf16le'))
-            console.log(await client.loadchunk(key2, res[0], Buffer.from(res[1], 'ascii').toString('utf8')))
-        }
-        //const response = await client.loadchunk(key2, dataIterator, data)
-        //console.log(response)
+        expect(res).to.equal('OK', `The response of load chunk with iterator ${chunk.iterator}`)
     });
 });
