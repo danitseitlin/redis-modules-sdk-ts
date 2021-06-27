8cd4cf32b901e835393af85201b767d89273dae2
Dani Tseitlin <39850374+danitseitlin@users.noreply.github.com>
2021-06-27 15:05:22 +0300

    [RediSearch] Add support for missing JSON attributes (#105)

--- tests/redisearch.ts
+++ tests/redisearch.ts
@@ -49,9 +49,25 @@ describe('RediSearch Module testing', async function() {
             weight: 2
         }])
         expect(response).to.equal('OK', 'The response of the FT.CREATE command');
+        response = await client.create(`${index}-json`, 'JSON', [{
+            name: '$.name',
+            type: 'TEXT',
+            as: 'name'
+        }])
+        expect(response).to.equal('OK', 'The response of the FT.CREATE command');
     });
     it('search function', async () => {
-        const response = await client.search(index, query)
+        let response = await client.search(index, query)
+        expect(response).to.equal(0, 'The response of the FT.SEARCH command')
+        response = await client.search(index, query, {
+            return: {
+                num: 3,
+                fields: [{
+                    name: '$.name',
+                    as: 'name'
+                }]
+            }
+        })
         expect(response).to.equal(0, 'The response of the FT.SEARCH command')
     });
     it('search function response test', async () => {
