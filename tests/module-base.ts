3f0c10297f0cfc1bf53c8b772277136e32e37528
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-07-14 17:39:50 +0300

    Adding support for Cluster and Redis in parallel (#112)

--- tests/module-base.ts
+++ tests/module-base.ts
@@ -1,30 +1,52 @@
 import { cliArguments } from 'cli-argument-parser';
 import { expect } from 'chai'
 import { Module } from '../modules/module.base';
-let client: Module;
-
-describe('AI testing', async function() {
+const clients: Module[] = []
+describe('Module base testing', async function() {
     before(async () => {
-        client = new Module('Module', {
+        clients.push(new Module('Module', {
             host: cliArguments.host,
             port: parseInt(cliArguments.port),
-        }, { isHandleError: false });
+        }, { isHandleError: false }));
+        /*
+        Commenting this out until we find a solution for the mock server.
+        clients.push(new Module('Module', [{
+            host: cliArguments.host,
+            port: parseInt(cliArguments.port),
+        }], { isHandleError: false }));*/
+        for(const client of clients)
+            await client.connect()
+    })
+    after(async() => {
+        for(const client of clients)
+            await client.disconnect()
+    })
+
+    it('sendCommand function', async() => {
+        for(const client of clients) {
+            let response = await client.sendCommand('set', ['foo', 'bar'])
+            expect(response).to.equal('OK', 'The response of the SET command')
+            response = await client.sendCommand('get', ['foo'])
+            expect(response).to.equal('bar', 'The response of the GET command')
+            response = await client.sendCommand('del', ['foo'])
+            expect(response).to.equal(1, 'The response of the DEL command')
+        }
     })
 
     it('handleResponse function', async () => {
         let response: any = 'OK';
-        let parsed = client.handleResponse(response)
+        let parsed = clients[0].handleResponse(response)
         expect(parsed).to.equal(response, 'The parsed response')
         response = ['key', 'value', 'key2', 'value2'];
-        parsed = client.handleResponse(response)
+        parsed = clients[0].handleResponse(response)
         expect(parsed.key).to.equal(response[1], 'The parsed response')
         expect(parsed.key2).to.equal(response[3], 'The parsed response')
         response = [
             'numbers', ['num1', 2]
         ];
-        parsed = client.handleResponse(response)
+        parsed = clients[0].handleResponse(response)
         expect(parsed.numbers.num1).to.equal(response[1][1], 'The parsed response')
-        console.log(client.handleResponse([
+        console.log(clients[0].handleResponse([
             'key',
             1,
             'fields',
@@ -34,7 +56,7 @@ describe('AI testing', async function() {
             ]
         ]))
 
-        console.log(client.handleResponse([ [ 'TERM', 'name', [] ] ]))
+        console.log(clients[0].handleResponse([ [ 'TERM', 'name', [] ] ]))
     });
 
     it('isOnlyTwoDimensionalArray function', async () => {
@@ -42,11 +64,11 @@ describe('AI testing', async function() {
             [1, 2, 3],
             1
         ]
-        expect(response).to.equal(false, 'If array is two dimensional')
+        expect(clients[0].isOnlyTwoDimensionalArray(response)).to.equal(false, 'If array is two dimensional')
         response = [
             [1, 2, 3],
             [6]
         ]
-        expect(response).to.equal(true, 'If array is two dimensional')
+        expect(clients[0].isOnlyTwoDimensionalArray(response)).to.equal(true, 'If array is two dimensional')
     })
 })
