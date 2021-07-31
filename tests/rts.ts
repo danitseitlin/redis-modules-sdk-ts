4ca454685862543d3f9878c60fc93b4d4372a55f
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-07-31 15:51:38 +0300

    .

--- tests/rts.ts
+++ tests/rts.ts
@@ -120,4 +120,8 @@ describe('RTS Module testing', async function() {
         const response = await client.queryindex('label=value')
         expect(response.length).eql(1, 'The response of the queryindex command');
     });
+    it('del function', async() => {
+        const res = await client.del(key1, date.toString(), new Date().toString())
+        console.log(res);
+    })
 });
