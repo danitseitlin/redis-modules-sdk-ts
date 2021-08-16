5d26d13c712b485835dcd84feec085f3febac626
Dani Tseitlin <dani.tseitlin@gmail.com>
2021-08-17 01:45:56 +0300

    .

--- modules/redisgraph.ts
+++ modules/redisgraph.ts
@@ -35,7 +35,7 @@ export class RedisGraph extends Module {
     async query(name: string, query: string, params?: {[key: string]: string}, run = true): Promise<void>/*Promise<string[][]>*/ {
         const args = this.buildQueryParams(name, query, params);
         //if(run)
-        return await this.sendCommand('GRAPH.QUERY', `'${args}'`)
+        return await this.sendCommand('GRAPH.QUERY', `'${args.join(" ")}'`)
         //return null;
     }
     buildQueryParams(name: string, query: string, params?: {[key: string]: string}): string[] {
