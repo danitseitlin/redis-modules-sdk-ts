89a0afdad9fc7f7f7502146a540ca4586650effe
Levente Novotny <Novotny.Levente@gmail.com>
2021-09-01 22:02:19 +0200

    Just to be safe

--- modules/module.base.ts
+++ modules/module.base.ts
@@ -111,39 +111,44 @@ export class Module {
             return this.handleResponse(response[0])
         }
         else if(isSearchQuery) {
-            //Search queries should be parsed into objects, if possible.
-            let responseObjects = response;
-            if(Array.isArray(response) && response.length % 2 === 1) {
-                // Put index as 0th element
-                responseObjects = [response[0]];
-                // Go through returned keys (doc:1, doc:2, ...)
-                for(let i = 1; i < response.length; i += 2) {
-                    // propertyArray is the key-value pairs eg: ['name', 'John']
-                    const propertyArray = response[i+1];
-                    responseObjects.push({
-                        key: response[i] //This is the key, 'eg doc:1'
-                    });
-                    
-                    if(Array.isArray(propertyArray) && propertyArray.length % 2 === 0) {
-                        for(let j = 0; j < propertyArray.length; j += 2){
-                            // Add keys to last responseObjects item
-                            // propertyArray[j] = key name
-                            // propertyArray[j+1] = value
-                            responseObjects[responseObjects.length - 1][propertyArray[j]] = propertyArray[j+1];
+            try {
+                //Search queries should be parsed into objects, if possible.
+                let responseObjects = response;
+                if(Array.isArray(response) && response.length % 2 === 1) {
+                    // Put index as 0th element
+                    responseObjects = [response[0]];
+                    // Go through returned keys (doc:1, doc:2, ...)
+                    for(let i = 1; i < response.length; i += 2) {
+                        // propertyArray is the key-value pairs eg: ['name', 'John']
+                        const propertyArray = response[i+1];
+                        responseObjects.push({
+                            key: response[i] //This is the key, 'eg doc:1'
+                        });
+
+                        if(Array.isArray(propertyArray) && propertyArray.length % 2 === 0) {
+                            for(let j = 0; j < propertyArray.length; j += 2){
+                                // Add keys to last responseObjects item
+                                // propertyArray[j] = key name
+                                // propertyArray[j+1] = value
+                                responseObjects[responseObjects.length - 1][propertyArray[j]] = propertyArray[j+1];
+                            }
                         }
                     }
                 }
-            }
-            //Check for a single dimensional array, these should only be keys, if im right
-            else if(response.every(entry => !Array.isArray(entry))) {
-                responseObjects = [response[0]];
-                for(let i = 1; i < response.length; i ++) {
-                    responseObjects.push({
-                        key: response[i],
-                    });
+                //Check for a single dimensional array, these should only be keys, if im right
+                else if(response.every(entry => !Array.isArray(entry))) {
+                    responseObjects = [response[0]];
+                    for(let i = 1; i < response.length; i ++) {
+                        responseObjects.push({
+                            key: response[i],
+                        });
+                    }
                 }
+                return responseObjects;
+            } catch(err) {
+                // console.log(this.handleError('An error occurred while parsing response\n' + err));
+                return response;
             }
-            return responseObjects;
         }
         else if(Array.isArray(response) && response.length > 1 && this.isOnlyTwoDimensionalArray(response)) {
             return this.handleResponse(this.reduceArrayDimension(response))
