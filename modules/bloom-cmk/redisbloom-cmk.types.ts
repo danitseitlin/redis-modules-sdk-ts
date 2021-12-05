8073be2cf8fa6f78edbe60025dfa5e715f99c48c
danitseitlin <dani.tseitlin@gmail.com>
2021-12-05 14:10:22 +0000

    Deploying to gh-pages from @ danitseitlin/redis-modules-sdk-ts@438c9a2d27cbc6a4e79f8403ac9a26ddf3365291 🚀

--- modules/bloom-cmk/redisbloom-cmk.types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The sets of the incrby items (and increments)
- * @param name The item name which counter to be increased.
- * @param increment The counter to be increased by this integer.
- */
-export type CMKIncrbyItems = {
-    name: string,
-    increment: number
-}
