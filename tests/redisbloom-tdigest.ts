8f31fa804469c6f0da6a85983ac5a782866feaba
Dani Tseitlin <dani.tseitlin@gmail.com>
2022-11-29 23:45:16 +0200

    .

--- tests/redisbloom-tdigest.ts
+++ tests/redisbloom-tdigest.ts
@@ -61,7 +61,7 @@ describe('RedisBloom TDigest filter testing', async function() {
         expect(response['Merged nodes']).to.eql(2, 'The merged nodes')
         expect(response['Unmerged nodes']).to.eql(0, 'The unmerged nodes')
         expect(response['Merged weight']).to.eql(2, 'The merged weight')
-        expect(response['Unmerged weight']).to.eql('0', 'The unmerged weight')
+        expect(response['Unmerged weight']).to.eql(0, 'The unmerged weight')
         expect(response['Total compressions']).to.eql(1, 'The total compressions')
     });
 });
